523a2c4480d66b21ca091afa34561feacc5eb066
yjhkdjsg <snehag2864@gmail.com>
2025-10-25 14:46:33 +0530

    Data pre-processing

--- components/NavBar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NavBar: React.FC = () => (
-  <nav className="w-full bg-blue-900 text-white p-4 flex justify-between items-center">
-    <span className="font-bold text-lg">Alzheimer's Detection System</span>
-  </nav>
-);
-
-export default NavBar;
